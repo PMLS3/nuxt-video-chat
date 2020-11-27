0fed58606bbff10844d7f399f82cc5699bec29e7
PMLS3 <peet@partnersinbiz.tech>
2020-11-27 07:44:12 +0200

    test composition

---
+++ pages/composition.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<script>
+import {
+  ref,
+  useContext,
+  computed,
+  onBeforeMount,
+  watch
+} from "@nuxtjs/composition-api";
+
+export default {
+  setup() {
+    const { store, $nuxtSocket } = useContext();
+    onBeforeMount(async () => {
+      console.log("useContext", useContext());
+      console.log("window.$nuxt", window.$nuxt);
+      console.log("nuxtSockett", $nuxtSocket);
+
+      let socket = $nuxtSocket({ channel: "/videochat" });
+    });
+  }
+};
+</script>
+
+<style></style>
